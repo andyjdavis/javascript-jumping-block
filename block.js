a7b1587af2e257ef67a287020aa2ef1dafadc020
Andrew Davis <andrew@moodle.com>
2013-07-17 17:45:52 +0800

    added a blurb at the top of the JS

--- block.js
+++ block.js
@@ -1,3 +1,13 @@
+/*
+ * A simple class that draws a block to the screen.
+ * The block is affected by gravity, can jump and changes shape as it moves.
+ *
+ * This code does not come with any sort of warranty.
+ * You are welcome to use it for whatever you like.
+ * A credit would be nice but is not required.
+ */
+
+
 function drawRect(context, x, y, width, height, color) {
     context.fillStyle = color;
     context.fillRect(x, y, width, height);
